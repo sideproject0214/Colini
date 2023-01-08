0efc0112f15d552da26e5feef47b5217576476ce
sideproject0214 <sideproject0214@gmail.com>
2023-01-09 07:34:05 +0900

    01.settings

---
+++ README.md
@@ -0,0 +1,3 @@
+# [Intro] Node Fullstack Guide
+
+## 01. setting
---
+++ src/db.js
@@ -0,0 +1,8 @@
+export const db = [
+  "https://bafybeidi7o3o3wiosdqidzrwbiwrgpm3jjltbw22caztuq2xdgtectoj6i.ipfs.w3s.link/man-shirts-1.jpg",
+  "https://w3s.link/ipfs/bafybeidujgajt4dby7ws6ii7mg5gkwwjddqoqyvsk4lwisufeupti4fg7i/man-shirts-2.png",
+  "https://w3s.link/ipfs/bafybeia245vduzzpgvsbpbsufjpadkrds3v6b3fu4yueyabtqvultycyxu/apparel-1.png",
+  "https://w3s.link/ipfs/bafybeicbjhyhmokbodeocaxfxahdl4gsxuqqkyeoqsvm46wzqmby7ehcba/footwear-1.png",
+  "https://w3s.link/ipfs/bafybeicfvkf34c4rrckfo7h2aipkbgbcnm442bxuot5irbpwoqg7shokfu/footwear-2.png",
+  "https://w3s.link/ipfs/bafybeidyottypwvecmqad4i7vk4gfvrkcghg7aoltr7yved2s2t6z5e2oi/woman-pants1.png",
+];
